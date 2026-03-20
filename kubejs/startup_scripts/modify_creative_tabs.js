8ac2fbf34b51ba88d1640a66d0eac57eccf359e9
BL4CK_ <160023261+imBL4CK@users.noreply.github.com>
2026-03-20 17:31:58 -0300

    - minor change to the texture of the moddedpedia in the inventory

--- kubejs/startup_scripts/modify_creative_tabs.js
+++ kubejs/startup_scripts/modify_creative_tabs.js
@@ -1,6 +1,7 @@
 StartupEvents.modifyCreativeTab("kubejs:tab", (e) => {
   e.icon = "techpack:basic_circuit";
   e.displayName = "Techpack";
+  e.add('guideme:guide[guideme:guide_id="techpack:moddedpedia"]')
 });
 StartupEvents.modifyCreativeTab("custommachinery:custom_machine", (e) => {
   //e.remove("custommachinery:custom_machine_item");
